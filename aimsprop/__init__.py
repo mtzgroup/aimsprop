99c1fca9388454db0c50bac06c18068953408334
Monika Williams <monikajwilliams@gmail.com>
2018-07-29 00:35:43 -0700

    updated __init__.py for aimspy parser

--- aimsprop/__init__.py
+++ aimsprop/__init__.py
@@ -1,5 +1,6 @@
 from .traj import *
 from .fms90 import *
+from .aimspy import *
 from .xyz import *
 from .geom import *
 from .blur import *
@@ -10,4 +11,6 @@ from .pop import *
 from .plot import *
 from .sine import *
 from .bootstrap import *
+from .pes import *
 import iam
+import vis_aims
