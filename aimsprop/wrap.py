299819180daefe040fddca2cbbb5697b9c399aeb
Monika Williams <monikajwilliams@gmail.com>
2018-11-07 15:27:44 -0800

    Added continuous sign option to wrapping function

--- aimsprop/wrap.py
+++ aimsprop/wrap.py
@@ -3,6 +3,7 @@ def unwrap_property(
     key,
     period,
     N=1,
+    match_all=False,
     ):
 
     """ Unwrap a periodic property to be maximally continuous through time for
@@ -14,13 +15,19 @@ def unwrap_property(
         key (str) - the name of the original property
         period (float) - the periodicity of the property
         N (int) - the number of images to try in +/- wings
+        match_all (bool) - whether to wrap all labels to same sign
     Result/Return:
         traj (Trajectory) - reference to the input Trajectory object. The
             property key is overwritten with the unwrapped property.
     """
 
+    if match_all:
+        ref_frame = traj.subset_by_label(traj.labels[0]).frames[0]
+    else:
+        ref_frame = None
+
     for label in traj.labels:
-        frame_old = None
+        frame_old = ref_frame
         for frame in traj.subset_by_label(label).frames:
             if frame_old is None: # Property has just started
                 frame_old = frame
